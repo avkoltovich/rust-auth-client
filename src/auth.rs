a77ca9371ab8d0ec08ddbbe4f285cac56780c2a0
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-12-06 20:51:31 +0300

    feat: добавлено еще одно поле в данные авторизации

--- src/auth.rs
+++ src/auth.rs
@@ -8,8 +8,9 @@ pub mod auth_module {
     #[derive(Deserialize)]
     pub struct AuthData {
         pub access_token: String,
-        pub refresh_token: String,
         pub expires_in: u32,
+        pub refresh_token: String,
+        pub jti: String,
         pub scope: String,
         pub user_id: u32,
         pub token_type: String,
