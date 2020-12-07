4bb1bb15ef706dff706922450330f8071da60407
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-12-07 18:48:30 +0300

    wip: дети у дерева создаются корректно

--- src/tree.rs
+++ src/tree.rs
@@ -3,30 +3,28 @@ use crate::requests::requests::NodeType;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct RawNode<'a> {
+pub struct RawNode {
     pub id: u32,
     #[serde(rename = "type")]
-    pub node_type: &'a NodeType,
+    pub node_type: NodeType,
     pub level: u32,
-    pub childs: Vec<RawNode<'a>>
+    pub childs: Vec<RawNode>
 }
 
 #[derive(Debug)]
-pub struct TreeNode<'a> {
+pub struct TreeNode {
     pub id: u32,
-    pub node_type: Box<&'a NodeType>,
     pub is_expanded: bool,
     pub is_checked: bool,
     pub is_indeterminate: bool,
     pub level: u32,
-    pub childs: Vec<TreeNode<'a>>
+    pub childs: Vec<TreeNode>
 }
 
-impl TreeNode<'a> {
-    pub fn new(node: Box<& RawNode>) -> TreeNode<'a> {
+impl TreeNode {
+    pub fn new(node: Box<& RawNode>) -> TreeNode {
         TreeNode {
             id: node.id,
-            node_type: Box::new(&node.node_type),
             level: node.level,
             is_checked: false,
             is_expanded: false,
@@ -36,7 +34,7 @@ impl TreeNode<'a> {
     }
 }
 
-pub fn build_tree(node: Box<&'static RawNode>) -> TreeNode {
+pub fn build_tree(node: Box<&RawNode>) -> TreeNode {
     let root = TreeNode::new(node);
 
 
