937d67ca181fe440e44885e3d226127d980e521a
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-12-07 20:10:59 +0300

    feat: теперь в TreeNode сохраняется ID родительской огранизации

--- src/tree.rs
+++ src/tree.rs
@@ -14,6 +14,7 @@ pub struct RawNode {
 #[derive(Debug)]
 pub struct TreeNode<'a> {
     pub id: u32,
+    pub parent_id: Option<u32>,
     pub node_type: &'a NodeType,
     pub is_expanded: bool,
     pub is_checked: bool,
@@ -23,21 +24,22 @@ pub struct TreeNode<'a> {
 }
 
 impl TreeNode<'_> {
-    pub fn new(node: Box<& RawNode>) -> TreeNode {
+    pub fn new(node: Box<& RawNode>, parent_id: Option<u32>) -> TreeNode {
         TreeNode {
             id: node.id,
+            parent_id,
             node_type: &node.node_type,
             level: node.level,
             is_checked: false,
             is_expanded: false,
             is_indeterminate: false,
-            childs: node.childs.iter().map(|child| TreeNode::new(Box::new(child))).collect()
+            childs: node.childs.iter().map(|child| TreeNode::new(Box::new(child), Some(node.id))).collect()
         }
     }
 }
 
 pub fn build_tree(node: Box<&RawNode>) -> TreeNode {
-    let root = TreeNode::new(node);
+    let root = TreeNode::new(node, None);
 
 
     root
